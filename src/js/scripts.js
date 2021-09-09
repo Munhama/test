3bb54199bc94dffab3dadefd9b8fb67ba8aea738
Vlad <johnytynky@gmail.com>
2021-09-09 15:47:31 +0700

    Add order section
    Added animations for the scheme icons. 
    Slider in the form changes the percentages.
    Add file button shows how many files have been selected.
    It remains to change the select menu.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,3 +15,30 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     })
 })
+
+// percent for slider
+let slider = document.getElementById("range");
+let output = document.getElementById("percent");
+output.innerHTML = slider.value;
+
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
+
+// add file button
+let inputs = document.querySelectorAll('.order__file');
+Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+    labelVal = label.querySelector('.order__file-text').innerText;
+
+    input.addEventListener('change', function (e) {
+    let countFiles = '';
+    if (this.files && this.files.length >= 1)
+        countFiles = this.files.length;
+
+    if (countFiles)
+        label.querySelector('.order__file-text').innerText = 'Выбрано файлов: ' + countFiles;
+    else
+        label.querySelector('.order__file-text').innerText = labelVal;
+    });
+});
